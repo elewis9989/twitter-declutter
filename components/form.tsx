59d1e89aa202b62963b407e79703cbda64bce1ae
elijah <elewis9989@gmail.com>
2022-08-20 13:46:56 -0400

    fix redirection

--- components/form.tsx
+++ components/form.tsx
@@ -33,7 +33,7 @@ const Form = () => {
     return (
       <button
         className='rounded-md bg-blue-400 flex items-center justify-center px-6 min-h-[2rem] hover:ring-2 ring-offset-blue-600'
-        onClick={() => signIn()}
+        onClick={() => signIn('twitter')}
       >
         <FaTwitter color='white' className='mx-1' />
         <p className='mx-1 text-white font-semibold'>Sign in</p>
